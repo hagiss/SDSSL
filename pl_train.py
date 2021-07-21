c5352a840ae9a41b1ad41d11b2e64a2b53517ac0
hagiss <66506438+hagiss@users.noreply.github.com>
2021-07-21 17:41:27 +0900

    remove xcit

--- pl_train.py
+++ pl_train.py
@@ -353,8 +353,7 @@ if __name__ == '__main__':
 
     # Model parameters
     parser.add_argument('--arch', default='vit_small', type=str,
-                        choices=['vit_tiny', 'vit_small', 'vit_base', 'xcit', 'deit_tiny', 'deit_small'] \
-                                + torchvision_archs + torch.hub.list("facebookresearch/xcit"),
+                        choices=['vit_tiny', 'vit_small', 'vit_base', 'xcit', 'deit_tiny', 'deit_small'] + torchvision_archs,
                         help="""Name of architecture to train. For quick experiments with ViTs,
             we recommend using vit_tiny or vit_small.""")
     parser.add_argument('--patch_size', default=16, type=int, help="""Size in pixels
