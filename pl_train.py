4a7eaa7b2f4d2b638ae7fd0317c0b00edb296890
hagiss <66506438+hagiss@users.noreply.github.com>
2021-10-24 22:45:07 +0900

    last_bn

--- pl_train.py
+++ pl_train.py
@@ -61,8 +61,8 @@ class PLLearner(pl.LightningModule):
 
         teacher.load_state_dict(student.state_dict())
 
-        self.student = NetWrapper(student, embed_dim, args, prediction=True, intermediate=self.st_inter, last_bn=False)
-        self.teacher = NetWrapper(teacher, embed_dim, args, prediction=False, intermediate=self.t_inter, last_bn=False)
+        self.student = NetWrapper(student, embed_dim, args, prediction=True, intermediate=self.st_inter, last_bn=True)
+        self.teacher = NetWrapper(teacher, embed_dim, args, prediction=False, intermediate=self.t_inter, last_bn=True)
 
         if self.st_inter != self.t_inter:
             self.teacher.projector.load_state_dict(self.student.projector[-1].state_dict())
