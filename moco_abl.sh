3a76b21fa24587567ccc883df85f1d2b71d66f1c
hagiss <66506438+hagiss@users.noreply.github.com>
2021-11-09 13:05:29 +0900

    moco Cabl

---
+++ moco_abl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python3 pl_train_moco.py --st_inter False --pred2 1.0 --const_ratio False --pred_loss 0.0
+python3 pl_train_moco.py --st_inter True --pred2 0.0 --const_ratio True --pred_loss 1.0
+python3 pl_train_moco.py --st_inter True --pred2 0.0 --const_ratio False --pred_loss 0.0
