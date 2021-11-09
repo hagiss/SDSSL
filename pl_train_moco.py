94dc34d65c3cdc2c861b8d5dbeeffbdf4555def6
hagiss <66506438+hagiss@users.noreply.github.com>
2021-11-09 21:08:08 +0900

    worker 4

--- pl_train_moco.py
+++ pl_train_moco.py
@@ -613,7 +613,7 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', '-e', type=int, default=300, help="epochs for scheduling")
     parser.add_argument('--max_epochs', type=int, default=300, help="epochs for actual training")
     parser.add_argument('--batch_size_per_gpu', '-b', type=int, default=512, help="batch size")
-    parser.add_argument('--num_workers', '-n', type=int, default=5, help='number of workers')
+    parser.add_argument('--num_workers', '-n', type=int, default=4, help='number of workers')
     parser.add_argument('--board_path', '-bp', default='./log', type=str, help='tensorboard path')
     parser.add_argument('--accumulate', default=1, type=int, help='accumulate gradient')
     parser.add_argument('--mlp_hidden', default=4096, type=int, help='mlp hidden dimension')
