64c4196b4c2456f8155cf986ec5fc5d0120ac4fb
hagiss <66506438+hagiss@users.noreply.github.com>
2021-11-11 16:37:07 +0900

    simclr_abl2

--- simclr_abl.sh
+++ simclr_abl.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 python3 pl_train_simclr.py --up 11 --same-view True
+python3 pl_train_simclr.py --up 11
 python3 pl_train_simclr.py --up 0
-python3 pl_train_simclr.py --up 3
-python3 pl_train_simclr.py --up 6
-python3 pl_train_simclr.py --up 9
