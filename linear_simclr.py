15abb3b349480a2bcb6b9e800b671438441a35cf
hagiss <66506438+hagiss@users.noreply.github.com>
2021-11-12 08:19:18 +0900

    asd

--- linear_simclr.py
+++ linear_simclr.py
@@ -545,8 +545,8 @@ if __name__ == '__main__':
     parser.add_argument('--mlp_hidden', default=4096, type=int, help='mlp hidden dimension')
     parser.add_argument('--ratio', default=1, type=float, help='loss ratio of layer2output')
     parser.add_argument('--up', default=12, type=int, help='layer2high skip layer')
-    parser.add_argument('--st_inter', default=False, type=bool, help='intermediate representation of student')
-    parser.add_argument('--t_inter', default=False, type=bool, help='intermediate representation of teacher')
+    parser.add_argument('--st_inter', default=False, type=utils.bool_flag, help='intermediate representation of student')
+    parser.add_argument('--t_inter', default=False, type=utils.bool_flag, help='intermediate representation of teacher')
 
     parser.add_argument('--data', '-d', metavar='DIR', default='../dataset',
                         help='path to dataset')
