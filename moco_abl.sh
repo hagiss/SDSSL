b39b6adb2f58eef99ad8fb9fd5150dea455a891a
hagiss <66506438+hagiss@users.noreply.github.com>
2021-11-12 21:45:20 +0900

    moco abl

--- moco_abl.sh
+++ moco_abl.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-python3 pl_train_moco.py --st_inter False --pred2 1.0 --const_ratio False --pred_loss 0.0
-python3 pl_train_moco.py --st_inter True --pred2 0.0 --const_ratio True --pred_loss 1.0
-python3 pl_train_moco.py --st_inter True --pred2 0.0 --const_ratio False --pred_loss 0.0
+python3 pl_train_moco.py --st_inter False --pred2 0.0 --const_ratio False --pred_loss 1.0 --max_epochs 100
+python3 pl_train_moco.py --st_inter True --pred2 0.0 --const_ratio True --pred_loss 1.0 --max_epochs 100
+python3 pl_train_moco.py --st_inter True --pred2 0.0 --const_ratio False --pred_loss 0.0 --max_epochs 100
+python3 pl_train_moco.py --st_inter True --pred2 0.0 --const_ratio False --pred_loss 1.0 --max_epochs 100
+python3 pl_train_moco.py --st_inter False --pred2 1.0 --const_ratio False --pred_loss 0.0 --max_epochs 100
