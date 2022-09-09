f938292e47538ff2d0a0dbf1fc43ea9ef520ed93
hagiss <66506438+hagiss@users.noreply.github.com>
2022-09-10 01:54:55 +0900

    asd

--- vision_transformer.py
+++ vision_transformer.py
@@ -287,6 +287,12 @@ def vit_base(patch_size=16, **kwargs):
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
+def vit_base(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4,
+        qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
 
 def deit_small(patch_size=16, **kwargs):
     model = VisionTransformer(
